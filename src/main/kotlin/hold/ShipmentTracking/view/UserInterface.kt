0a563fa7eb021a1ae70e595f354feda267623107
Rylei Mindrum <rymindrum@gmail.com>
2024-07-07 19:54:17 -0600

    ui without start() imp.

--- src/main/kotlin/hold/ShipmentTracking/view/UserInterface.kt
+++ src/main/kotlin/hold/ShipmentTracking/view/UserInterface.kt
@@ -1,4 +1,11 @@
-package org.example.hold.ShipmentTracking.view
-
 class UserInterface {
+    private val viewHelper = TrackerViewHelper()
+
+    fun start() {
+        //ui setup and user input
+    }
+
+    fun trackShipment(id: String) {
+        viewHelper.trackShipment(id)
+    }
 }
