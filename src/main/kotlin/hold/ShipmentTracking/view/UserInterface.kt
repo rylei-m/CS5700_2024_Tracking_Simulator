677a51743fdd972d6c56214a2a194b4cfa30a9d1
Rylei Mindrum <rymindrum@gmail.com>
2024-07-07 20:28:06 -0600

    update UI.kt added some logic for start()

--- src/main/kotlin/hold/ShipmentTracking/view/UserInterface.kt
+++ src/main/kotlin/hold/ShipmentTracking/view/UserInterface.kt
@@ -2,7 +2,27 @@ class UserInterface {
     private val viewHelper = TrackerViewHelper()
 
     fun start() {
-        //ui setup and user input
+        while (true) {
+            println("Enter a command (track, stop, exit):")
+            when (readLine()?.trim()) {
+                "track" -> {
+                    println("Enter shipment ID for tracking:")
+                    val id = readLine()?.trim()
+                    if (id != null) {
+                        viewHelper.trackShipment(id)
+                    }
+                }
+                "stop" -> {
+                    println("Enter shipment ID to stop tracking:")
+                    val id = readLine()?.trim()
+                    if (id != null) {
+                        viewHelper.stopTrackingShipment(id)
+                    }
+                }
+                "exit" -> return
+                else -> println("Invalid command.")
+            }
+        }
     }
 
     fun trackShipment(id: String) {
