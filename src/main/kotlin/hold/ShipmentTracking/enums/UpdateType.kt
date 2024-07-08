e42cd69036f1c32999694e9b1a0882dfaaa5d95d
Rylei Mindrum <rymindrum@gmail.com>
2024-07-07 19:57:55 -0600

    refactored. code for shipment was in wrong file. moved data class shipment to shipment file.

--- src/main/kotlin/hold/ShipmentTracking/enums/UpdateType.kt
+++ src/main/kotlin/hold/ShipmentTracking/enums/UpdateType.kt
@@ -8,12 +8,3 @@ enum class UpdateType {
     CANCELED,
     NOTEADDED
 }
-
-data class Shipment(
-    val id: String,
-    var status: String,
-    var location: String?,
-    var expectedDelivery: Long?,
-    val notes: MutableList<String> = mutableListOf(),
-    val updates: MutableList<String> = mutableListOf(),
-)
--- src/main/kotlin/hold/ShipmentTracking/models/Shipment.kt
+++ src/main/kotlin/hold/ShipmentTracking/models/Shipment.kt
@@ -1,4 +1,10 @@
 package org.example.hold.ShipmentTracking.models
 
-class Shipment {
-}
+data class Shipment(
+    val id: String,
+    var status: String,
+    var location: String?,
+    var expectedDelivery: Long?,
+    val notes: MutableList<String> = mutableListOf(),
+    val updates: MutableList<String> = mutableListOf(),
+)
