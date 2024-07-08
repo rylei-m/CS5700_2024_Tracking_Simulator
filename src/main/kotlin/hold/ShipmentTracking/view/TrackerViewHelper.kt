a3ab7c7916036781461be1bf6ffe35d081bc43c2
Rylei Mindrum <rymindrum@gmail.com>
2024-07-07 19:52:15 -0600

    class that prepares the data for the UI. need to change trackshipment to use elvis instead of non-null(!!) and implement updateUI

--- src/main/kotlin/hold/ShipmentTracking/view/TrackerViewHelper.kt
+++ src/main/kotlin/hold/ShipmentTracking/view/TrackerViewHelper.kt
@@ -1,4 +1,26 @@
-package org.example.hold.ShipmentTracking.view
-
 class TrackerViewHelper {
+    private val trackedShipments = mutableMapOf<String, Shipment>()
+
+    init {
+        TrackingSimulator.addObserver {
+            shipment ->
+            if (trackedShipments.containsKey(shipment.id)) {
+                trackedShipments[shipment.id] = shipment
+                updateUI(shipment)
+            }
+        }
+    }
+
+    fun trackShipment(id: String) {
+        trackedShipments[id] = TrackingSimulator.getShipment(id)
+        updateUI(trackedShipments[id]!!) //!! is non-null assertion maybe use elvis instead?
+    }
+
+    fun stopTrackingShipment(id: String) {
+        trackedShipments.remove(id)
+    }
+
+    private fun updateUI(shipment: Shipment) {
+        // do this later :,(
+    }
 }
