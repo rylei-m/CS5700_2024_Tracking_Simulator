4355c5b3c5a0d6603e94369ee0c00b9e52292ae0
Rylei Mindrum <rymindrum@gmail.com>
2024-07-07 19:26:39 -0600

    defined enum class and Shipment data class

--- src/main/kotlin/hold/ShipmentTracking/enums/UpdateType.kt
+++ src/main/kotlin/hold/ShipmentTracking/enums/UpdateType.kt
@@ -1,4 +1,19 @@
-package org.example.hold.ShipmentTracking.enums
-
-class UpdateType {
+enum class UpdateType {
+    CREATED,
+    SHIPPED,
+    LOCATION,
+    DELIVERED,
+    DELAYED,
+    LOST,
+    CANCELED,
+    NOTEADDED
 }
+
+data class Shipment(
+    val id: String,
+    var status: String,
+    var location: String?,
+    var expectedDelivery: Long?,
+    val notes: MutableList<String> = mutableListOf(),
+    val updates: MutableList<String> = mutableListOf(),
+)
